3d5709a5314577dc028a1f4fb95bd7f60e6621fd
ravivish <ravivishwakarma825@gmail.com>
2021-08-30 00:08:55 +0530

    Project initialized

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = { course: {}, loading: true };
+  //   // console.log(props.match.params.id);
+  // }
+
+  render() {
+    return (
+      <div className="footer-links">
+        <div className="first-col link-columns">
+
+        </div>
+        <ul className="second-col link-columns">
+          <li>Udemy Business</li>
+          <li>Teach On Udemy</li>
+          <li>Get the App</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+        <ul className="third-col link-columns">
+          <li>Career</li>
+          <li>Blog</li>
+          <li>Help and Support</li>
+          <li>Affiliate</li>
+        </ul>
+        <ul className="fourth-col link-columns">
+          <li>Teams</li>
+          <li>Privacy Policy</li>
+          <li>Sitemap</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Footer;
